5cc927f69e92d298910883ad6d8ef0301c859f1f
whitechapel007 <ebukareed@gmail.com>
2021-09-28 15:28:40 +0100

    edited the hands

---
+++ src/components/firsthand/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import image1 from "../images/hand.svg"
+import "./style.css"
+
+function index() {
+    return (
+        <div className="div-1">
+        <img src={image1} alt="" className="img-1" />
+    </div>
+    )
+}
+
+export default index
--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,18 +1,16 @@
 import React from 'react'
-import image1 from "../../components/images/hand.svg"
-import image2 from "../../components/images/hand2.svg"
 import "../../App.css";
 import {Label} from "../../components/inputField/index"
 import InputField from '../../components/inputField';
+import Firsthand from "../../components/firsthand"
+import Secondhand from "../../components/secondhand"
 
 
 function index() {
     return (
         <div className="container">
 
-        <div className="div-1">
-            <img src={image1} alt="" className="img-1" />
-        </div>
+       <Firsthand className="div-1"/>
 
         <section className="input-section">
 <h1 className="input-heading">
@@ -30,10 +28,7 @@ Get started and access our gadget insurance and loan services at a go
 </form>
         </section>
 
-        <div className="div-2">    
-<img src={image2} alt="" className="img-2" />
-        </div>
-
+       <Secondhand className="div-2"/>
         </div>
     )
 }
