206698d1a31e90a0909f5d9f6387fd93904f7be4
whitechapel007 <ebukareed@gmail.com>
2021-09-29 12:50:20 +0100

    finishing touches on mobile view

---
+++ src/pages/login/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "../../App.css";
+import InputField from '../../components/inputField';
+import Firsthand from "../../components/firsthand"
+import Secondhand from "../../components/secondhand"
+import Container from '../../components/Container';
+import Button from '../../components/button';
+
+
+function index() {
+    return ( <
+        Container >
+        <
+        section >
+
+
+        <
+        /section>
+
+
+        <
+        /Container>
+    )
+}
+
+export default index
